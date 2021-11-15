87a0da5c78d6883cd14dd3aed53496a177b46c7a
JBatsyus <ju.kalabina@gmail.com>
2021-11-15 12:45:47 +0200

    внесла изменения в настойки линтера,  форматировала components

--- card_homework/src/App.js
+++ card_homework/src/App.js
@@ -1,32 +1,32 @@
-import './App.css';
-import Card from './components/Card';
-import { cards } from './components/Cards';
+import "./App.css";
+import Card from "./components/Card";
+import { cards } from "./components/Cards";
 
 function App() {
-  return (
-    <div className="App">
-      {cards.map(oneCard => (
-        <Card
-          key={oneCard.id}
-          header={oneCard.header}
-          blueВackgroundHeader={oneCard.blueВackgroundHeader}
-          greenВackgroundHeader={oneCard.greenВackgroundHeader}
-          redВackgroundHeader={oneCard.redВackgroundHeader}
-          blackВackgroundHeader={oneCard.blackВackgroundHeader}
-          rubles={oneCard.rubles}
-          sum={oneCard.sum}
-          month={oneCard.month}
-          text={oneCard.text}
-          footer={oneCard.footer}
-          isSelected={oneCard.isSelected}
-          colorBlueCard={oneCard.colorBlueCard}
-          colorGreenCard={oneCard.colorGreenCard}
-          colorRedCard={oneCard.colorRedCard}
-          colorBlackCard={oneCard.colorBlackCard}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="App">
+            {cards.map(oneCard => (
+                <Card
+                    key={oneCard.id}
+                    header={oneCard.header}
+                    blueВackgroundHeader={oneCard.blueВackgroundHeader}
+                    greenВackgroundHeader={oneCard.greenВackgroundHeader}
+                    redВackgroundHeader={oneCard.redВackgroundHeader}
+                    blackВackgroundHeader={oneCard.blackВackgroundHeader}
+                    rubles={oneCard.rubles}
+                    sum={oneCard.sum}
+                    month={oneCard.month}
+                    text={oneCard.text}
+                    footer={oneCard.footer}
+                    isSelected={oneCard.isSelected}
+                    colorBlueCard={oneCard.colorBlueCard}
+                    colorGreenCard={oneCard.colorGreenCard}
+                    colorRedCard={oneCard.colorRedCard}
+                    colorBlackCard={oneCard.colorBlackCard}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default App;
--- card_homework/src/components/Card.jsx
+++ card_homework/src/components/Card.jsx
@@ -1,55 +1,66 @@
-import './Card.scss';
+import "./Card.scss";
 
 const Card = props => {
-  const {
-    isSelected,
-    header,
-    text,
-    sum,
-    colorBlueCard,
-    colorGreenCard,
-    colorRedCard,
-    colorBlackCard,
-    blueВackgroundHeader,
-    greenВackgroundHeader,
-    redВackgroundHeader,
-    blackВackgroundHeader,
-  } = props;
+    const {
+        isSelected,
+        header,
+        text,
+        sum,
+        colorBlueCard,
+        colorGreenCard,
+        colorRedCard,
+        colorBlackCard,
+        blueВackgroundHeader,
+        greenВackgroundHeader,
+        redВackgroundHeader,
+        blackВackgroundHeader,
+    } = props;
 
-  const blueCard = colorBlueCard ? 'colorBlue' : '';
-  const greenCard = colorGreenCard ? 'colorGreen' : '';
-  const redCard = colorRedCard ? 'colorRed' : '';
-  const blackCard = colorBlackCard ? 'colorBlack' : '';
-  const thisSelected = isSelected ? ' selected' : '';
+    const blueCard = colorBlueCard ? "colorBlue" : "";
+    const greenCard = colorGreenCard ? "colorGreen" : "";
+    const redCard = colorRedCard ? "colorRed" : "";
+    const blackCard = colorBlackCard ? "colorBlack" : "";
+    const thisSelected = isSelected ? " selected" : "";
 
-  const blueBack = blueВackgroundHeader ? ' colorHeaderBlue' : '';
-  const greenBack = greenВackgroundHeader ? ' colorHeaderGreen' : '';
-  const redBack = redВackgroundHeader ? ' colorHeaderRed' : '';
-  const blackBack = blackВackgroundHeader ? ' colorHeaderBlack' : '';
+    const blueBack = blueВackgroundHeader ? " colorHeaderBlue" : "";
+    const greenBack = greenВackgroundHeader ? " colorHeaderGreen" : "";
+    const redBack = redВackgroundHeader ? " colorHeaderRed" : "";
+    const blackBack = blackВackgroundHeader ? " colorHeaderBlack" : "";
 
-  return (
-    <div className="cardContainer">
-      <div
-        className={
-          'card ' + blueCard + greenCard + redCard + thisSelected + blackCard
-        }
-      >
-        <div
-          className={'card-header' + blueBack + greenBack + redBack + blackBack}
-        >
-          {header}{' '}
+    return (
+        <div className="cardContainer">
+            <div
+                className={
+                    "card " +
+                    blueCard +
+                    greenCard +
+                    redCard +
+                    thisSelected +
+                    blackCard
+                }
+            >
+                <div
+                    className={
+                        "card-header" +
+                        blueBack +
+                        greenBack +
+                        redBack +
+                        blackBack
+                    }
+                >
+                    {header}{" "}
+                </div>
+                <div className="card-body">
+                    <div className="card-rubles">руб</div>
+                    <div className="card-sum">{sum}</div>
+                    <div className="card-month ">/мес</div>
+                </div>
+                <div className="card-text">{text}</div>
+                <div className="card-footer">
+                    Объем включенного трафика не ограничен
+                </div>
+            </div>
         </div>
-        <div className="card-body">
-          <div className="card-rubles">руб</div>
-          <div className="card-sum">{sum}</div>
-          <div className="card-month ">/мес</div>
-        </div>
-        <div className="card-text">{text}</div>
-        <div className="card-footer">
-          Объем включенного трафика не ограничен
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default Card;
